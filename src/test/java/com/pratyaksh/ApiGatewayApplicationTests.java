e4d324abb7f5894ecdfa8a8017896ba6a3b320bf
Pratyaksh-hub <pratyakshm111@gmail.com>
2025-03-09 00:55:39 +0530

    first commit

---
+++ src/test/java/com/pratyaksh/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratyaksh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
